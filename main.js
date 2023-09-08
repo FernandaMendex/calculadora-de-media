bbcc7cc507e8dc12693eca56b3576fdf8e284096
FernandaMendex <fernandamendesq@hotmail.com>
2023-09-07 21:48:40 -0300

    feat: adição da funcionalidade de solicitar ao usuário média mínima para o cálculo dos dados inseridos no formulário

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
+const notaMinima = parseFloat(prompt("Insira média mínima para aprovação:"));
+
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
@@ -32,7 +34,7 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputTituloAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
         linha += '</tr>';
 
         linhas += linha;
@@ -51,7 +53,7 @@ function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal(){
